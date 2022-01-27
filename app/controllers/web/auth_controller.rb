d5d54f1542edb2eb63dc712a2d6185c3b4208778
Dmitry Kabanov <dkabanov6@gmail.com>
2022-01-27 17:23:10 +0500

    added login and logout

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -5,16 +5,24 @@ class Web::AuthController < Web::ApplicationController
 
   def callback
     auth_user_info = auth[:info]
+    auth_user_credentials = auth[:credentials]
 
     user = User.find_or_initialize_by(email: auth_user_info[:email].downcase)
-    user.first_name = auth_user_info[:name]
+
+    user.nickname = auth_user_info[:nickname]
+    user.token = auth_user_credentials[:token]
 
     if user.save
       sign_in user
-      redirect_to root_path, notice: t('messages.welcome_user', user: user.email)
+      redirect_to root_path, notice: t('messages.welcome_user', user: user.nickname)
     else
       redirect_to new_session_path, notice: t('messages.inсorrect_user')
     end
+  end
+
+  def logout
+    sign_out
+    redirect_to root_path, notice: t('messages.goodby')
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
     get '/auth/:provider/callback', to: 'auth#callback', as: :callback_auth, via: :all
     post '/auth/:provider', to: 'auth#request', as: :auth_request
+    delete '/auth/logout', to: 'auth#logout', as: :auth_logout
   end
 end
