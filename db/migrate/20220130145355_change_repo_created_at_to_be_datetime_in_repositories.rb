f81e7313689a36fb082bf729fe4096bd2522b0f8
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-06 19:31:34 +0500

    remove migrations

--- db/migrate/20220130145355_change_repo_created_at_to_be_datetime_in_repositories.rb
+++
@@ -1,5 +0,0 @@
-class ChangeRepoCreatedAtToBeDatetimeInRepositories < ActiveRecord::Migration[6.1]
-  def change
-    change_column :repositories, :repo_created_at, :datetime
-  end
-end
--- db/migrate/20220130145530_change_repo_updated_at_to_be_datetime_in_repositories.rb
+++
@@ -1,5 +0,0 @@
-class ChangeRepoUpdatedAtToBeDatetimeInRepositories < ActiveRecord::Migration[6.1]
-  def change
-    change_column :repositories, :repo_updated_at, :datetime
-  end
-end
