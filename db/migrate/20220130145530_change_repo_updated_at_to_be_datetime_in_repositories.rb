01f1975bc4919d71cd1d4a7fe615a2bbbd5213c7
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-06 19:42:33 +0500

    updated migrations

--- db/migrate/20220130145530_change_repo_updated_at_to_be_datetime_in_repositories.rb
+++ db/migrate/20220130145530_change_repo_updated_at_to_be_datetime_in_repositories.rb
@@ -1,5 +1,5 @@
 class ChangeRepoUpdatedAtToBeDatetimeInRepositories < ActiveRecord::Migration[6.1]
   def change
-    change_column :repositories, :repo_updated_at, :datetime
+    change_column :repositories, :repo_updated_at, :datetime, using: 'repo_updated_at::timestamp without time zone'
   end
 end
