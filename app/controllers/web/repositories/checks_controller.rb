4a01628dcbd3ae03131dc1ee8a101c654318e6bc
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-11 17:41:41 +0500

    fix error

--- app/controllers/web/repositories/checks_controller.rb
+++ app/controllers/web/repositories/checks_controller.rb
@@ -26,6 +26,5 @@ class Web::Repositories::ChecksController < ApplicationController
     if @check.save
       RepositoryCheckJob.perform_later @repository.id, @check.id
     end
-    else
   end
 end
