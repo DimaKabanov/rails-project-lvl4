21650ce81f7538aa7a8a4b64fca626f97e67d027
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-14 06:31:39 +0500

    remove hooks

--- app/jobs/update_info_repository_job.rb
+++ app/jobs/update_info_repository_job.rb
@@ -22,21 +22,17 @@ class UpdateInfoRepositoryJob < ApplicationJob
 
     CheckRepositoryJob.perform_later(repository_id)
 
-    begin
-      client.create_hook(
-        repository.github_id,
-        'web',
-        {
-          url: api_checks_url,
-          content_type: 'json'
-        },
-        {
-          events: ['push'],
-          active: true
-        }
-      )
-    rescue StandardError => e
-      e
-    end
+    # client.create_hook(
+    #   repository.github_id,
+    #   'web',
+    #   {
+    #     url: api_checks_url,
+    #     content_type: 'json'
+    #   },
+    #   {
+    #     events: ['push'],
+    #     active: true
+    #   }
+    # )
   end
 end
