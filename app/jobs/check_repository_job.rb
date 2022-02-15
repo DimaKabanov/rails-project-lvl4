121ca1db1f8b3541690921c496b3d56017ec9c2b
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-15 06:34:45 +0500

    added loging error

--- app/jobs/check_repository_job.rb
+++ app/jobs/check_repository_job.rb
@@ -51,7 +51,8 @@ class CheckRepositoryJob < ApplicationJob
 
       check.finish!
       CheckMailer.with(check: check).check_success_email.deliver_now
-    rescue StandardError
+    rescue StandardError => e
+      pp e
       check.reject!
       CheckMailer.with(check: check).check_error_email.deliver_now
     end
