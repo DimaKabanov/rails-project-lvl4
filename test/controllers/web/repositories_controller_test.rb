1bfa5dd59270f675d58fecb1a67b8440c2d4f06e
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-16 15:36:10 +0500

    added tests

--- test/controllers/web/repositories_controller_test.rb
+++ test/controllers/web/repositories_controller_test.rb
@@ -6,10 +6,36 @@ class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
     sign_in_as_user @user
+    @attrs = {
+      github_id: '1'
+    }
   end
 
   test 'should get index' do
     get repositories_path
     assert_response :success
+  end
+
+  test 'should get show' do
+    repository = repositories :updated
+    get repository_path repository
+    assert_response :redirect
+  end
+
+  test 'should get new' do
+    get new_repository_path
+    assert_response :success
+  end
+
+  test 'should create new repo' do
+    post repositories_url, params: {
+      repository: @attrs
+    }
+
+    repository = Repository.find_by! github_id: @attrs[:github_id]
+
+    assert_redirected_to repositories_path
+    assert { repository }
+    assert_enqueued_with job: UpdateInfoRepositoryJob
   end
 end
