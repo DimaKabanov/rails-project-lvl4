bddbc7a878be40cd534c7a439e806a01c7165255
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-16 09:22:56 +0500

    change migration

--- db/migrate/20220214151249_change_github_id_to_be_string_in_repositories.rb
+++ db/migrate/20220214151249_change_github_id_to_be_string_in_repositories.rb
@@ -1,5 +1,5 @@
 class ChangeGithubIdToBeStringInRepositories < ActiveRecord::Migration[6.1]
   def change
-    change_column :repositories, :github_id, :string, using: 'github_id::string'
+    change_column :repositories, :github_id, :string
   end
 end
