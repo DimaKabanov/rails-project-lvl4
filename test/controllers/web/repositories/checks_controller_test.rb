e7375d21d9064c6fae70cfe456917686fd8de417
Dmitry Kabanov <dkabanov6@gmail.com>
2022-02-16 17:56:56 +0500

    added check test

--- test/controllers/web/repositories/checks_controller_test.rb
+++ test/controllers/web/repositories/checks_controller_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class Web::Repositories::ChecksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    sign_in_as_user @user
+  end
+
+  test 'should get show' do
+    repository = repositories :updated
+    check = repository_checks :updated
+
+    get repository_check_path repository, check
+    debugger
+    assert_response :success
+  end
 end
